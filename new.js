f89c8ffab7a7afa83e1d74854b9754f078fa79ef
Opeoluwa-Ogunlaja <ogunlajafaith@gmail.com>
2023-03-08 20:17:27 +0100

    Added my solution for the solver

--- new.js
+++ new.js
@@ -3,3 +3,9 @@ var b = 3
 var c = 2
 var QE = (-1*b)+Math.sqrt((b*b)-(4*a*c))/2*a 
 console.log(QE)
+
+// Ope's Changes
+const x1 = ((-1 * b) + Math.sqrt((b*b) - (4*a*c)))/(2*a)
+const x2 = ((-1 * b) - Math.sqrt((b*b) - (4*a*c)))/(2*a)
+
+console.log(x1, x2);
